e7ed9537998d3983aebdacabe4dba50416483405
Weslly Viana <devweslly@gmail.com>
2022-06-18 13:58:58 -0300

    App Modelo - Criando Fragments

--- app/src/main/java/com/exemple/appmodelo/MainActivity.kt
+++ app/src/main/java/com/exemple/appmodelo/MainActivity.kt
@@ -82,8 +82,8 @@ class MainActivity : AppCompatActivity() {
         * 2) Colocar gráfico de navegação em relação ao container (NavHostFragment)
         * 3) Acessar navController para fazer navegação
 
-        * app --> new --> Android Resource File
-        * name: nav_graph | resource type: Navigation --> OK
+        * 1) app --> new --> Android Resource File
+        * 2) name: nav_graph | resource type: Navigation --> OK
 
         * Adicionando novos destinos ao programa (add distination)
         * Primeiro Fragment - Create new destination --> Fragment Blank --> Name: listFragment --> Finish
@@ -97,7 +97,7 @@ class MainActivity : AppCompatActivity() {
         * Para ele criar uma nova tela de listFragment e carregar a lista novamente (criando do zero)
         * (matando os processos list e form anteriores e criando um novo list)
         * popUpToInclusive: True
-        * Entrando na unidade dinâmica e montando o layout (frafment_list e fragment_form)
+        * Entrando na unidade dinâmica e montando o layout (fragment_list e fragment_form)
         * Na activity_main (ou o container onde os fragments irão ficar)
         * E será criado o NavHostFragment (apenas jogar ele na ViewGroup do no container)
         * Será preciso ter um nav_graph para o NavHostFragment existir --> Basta pegar o nav_graph criado
@@ -105,4 +105,39 @@ class MainActivity : AppCompatActivity() {
         * O primeiro fragment que aparece será o da lista (pois foi definido assim no nav_graph - simbolo da casinha)
 
         * Fazendo com que os fragments consiga acessar o NavController e fazer a navegação entre eles
+
+        * Dentro da parte lógica (FormFragment e ListFragment)
+
+        * Para fazer a navegação dos fragments funcionarem vamos explorar as unidade lógica (FormFragment e ListFragment)
+        * Ao construir o fragment é gerado a unidade dinâmica (layouts) e unidade lógica
+        * Não precisamos colocar os fragments dentro do manifest para fazer a navegação entre telas
+        * (como é feito com as activity usando intents)
+        * Dentro da parte lógica (ListFragment e FormFragment) usaremos apenas o
+        * OnCreatView para instanciar (criar) nossas coisas.
+        * Portanto vamos limpar tudo e mantendo apenas o OnCreatView
+
+        * Dentro do método OnCreatView
+
+        * O nosso fragment retorna para a gente conseguir pegar a unidade dinâmica
+        * Ele retorna nossa view a partir do inflater.
+        * Para o nosso xml conseguir aparecer para gente, é preciso inflar ele.
+        * Entao para ser definido que o listFragment vai receber a view, é preciso inflar a view e retornar ele dentro
+        * do método OnCreatView
+        * Dentro do fragment não é possível usar o findViewById sem ser dentro de um layout específico (dentro de uma view)
+        * Jogar nossa objeto inflater em uma val view. E retornar ela mesma em return
+        * Para se feito a navegação utilizando o Nav Controller, vamos referenciar nosso primeiro componente
+        * dentro de uma val floatingAdd (vamos utilizar botao que fica no listFragment na unidade dinâmica)
+        * vamos pegar a val view e colocar o fidViewById do tipo <FloatingActionButton> para o id floatingAdd
+        * Agora temos este componente referênciado utilizando a nossa view
+        * A partir disso podemos usar o metodo para click utilizando nossa val floatingAdd
+        * para navegar para o proximo fragment por meio da ação criado no nosso nav_graph: action_listFragment_to_infoFragment
+        * Para achar o nav controller e fazer esta navegação vamos usar o metodo: findNavController()
+        * Este metodo vai verificar quem esta cuidando da navegação (que esta atrelado ao nav_graph)
+        * Em seguida usamos o metodo: .navigate. E a partir dele iremos passar o id que indica nossa navgação
+        * findNavController().navigate(R.id.action_listFragment_to_formFragment)
+
+        * Este mesmo processo é usado para o formFragment, mas usando o button salvar
+
+    *** Criando o Recycler View ***
+        * Criando o layout
 */
